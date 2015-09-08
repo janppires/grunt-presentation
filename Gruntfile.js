4e3c223e7af9963db0ff3aeec1eca45aeee0b9fc
Jorge Pires <janppires@gmail.com>
2015-09-09 00:35:46 +0100

    step-3: initial setup

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,11 +3,28 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
       connect: {
-        my_server_with_defaults: {}
+        options:{
+          keepalive:true
+        },
+        my_server_with_defaults: {
+          // http://localhost:8000/app
+        },
+        my_server_customized: {
+          // http://localhost:9000
+          options: {
+            port: 9000,
+            base: ['.', 'app'],
+            protocol: 'http',
+            hostname: '0.0.0.0',
+            debug: false
+          }
+        }
       }
   });
 
   // Load the plugin that provides the "connect" task.
   grunt.loadNpmTasks('grunt-contrib-connect');
 
+  grunt.registerTask('default', ['connect:my_server_with_defaults']);
+  grunt.registerTask('custom', ['connect:my_server_customized']);
 };
